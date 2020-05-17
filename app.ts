615931b78475b47b670de82adb874042c5971515
ismet kizgin <info@ismetkizgin.com>
2020-05-17 04:34:39 +0300

    feat: add app.ts

---
+++ app.ts
@@ -0,0 +1,22 @@
+function BubbleSort(array: number[]) {
+    let len: number = array.length;
+    for (let i: number = 0; i < len; i++) {
+        for (let j: number = 0; j < len; j++) {
+            if (array[j] > array[j + 1]) {
+                let tmp: number = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+    return array;
+}
+
+let rndNumberArray: number[] = new Array(10);
+
+for (let index = 0; index < 10; index++) {
+    rndNumberArray[index] = Math.floor((Math.random() * 1000) + 1)
+}
+console.log(`\nRandom Number Array Listesi: ${rndNumberArray} \n`);
+
+console.log(`Array Sorting List: ${BubbleSort(rndNumberArray)}\n`);
